b0a32f7cbbf944a9e3928965b164fc68989c8784
Jayesh Deorukhkar <52153715+Jayesh2812@users.noreply.github.com>
2021-12-22 17:59:13 +0530

    time tracking dashboard complete

---
+++ time-tracking-dashboard-main/app.js
@@ -0,0 +1,58 @@
+const getData = (frequency) => {
+    fetch("data.json")
+        .then((res) => res.json())
+        .then((data) => {
+            data = data.map((item) => ({
+                title: item.title,
+                activity_data: item.timeframes[frequency],
+            }));
+            setActivities(data);
+        });
+};
+const setActivities = (data) => {
+    activities.innerHTML = "";
+    let images = [
+        './images/icon-work.svg',
+        './images/icon-play.svg',
+        './images/icon-study.svg',
+        './images/icon-exercise.svg',
+        './images/icon-social.svg',
+        './images/icon-self-care.svg',
+    ]
+    data.forEach(({ title, activity_data: { current, previous } }, index) => {
+        console.log(index)
+        activities.innerHTML += `
+        <div class="activity-wrapper">
+
+            <img src=${images[index]} alt="" class="activity-wrapper-img" />
+            
+            <div class="activity">
+                <p class="activity-name">${title}
+                    <img src="./images/icon-ellipsis.svg" alt="ellipsis" class="activity-more-options" />
+                </p>
+                <h1 class="activity-current">${current}hrs</h1>
+                <p class="activity-previous">Last week - ${previous}hrs</p>
+            </div>
+        </div>
+        `;
+    });
+};
+let active_btn = document.querySelector(".active");
+let frequency = active_btn.getAttribute("aria-data");
+let activities = document.querySelector(".activities");
+let btns = document.querySelectorAll(".profile-frequency");
+getData(frequency);
+
+btns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        let new_frequency = e.target.getAttribute("aria-data");
+        if (frequency !== new_frequency) {
+            console.log(e.target);
+            document.querySelector(".active").classList.remove("active");
+            e.target.classList.add("active");
+
+            frequency = new_frequency;
+            getData(frequency);
+        }
+    });
+});
