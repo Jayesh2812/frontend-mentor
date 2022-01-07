3d254a1f3c580d37136d7f095bcd5b5f03d4c710
Jayesh Deorukhkar <52153715+Jayesh2812@users.noreply.github.com>
2022-01-08 00:08:32 +0530

    url-shortener-finished

--- url-shortening-api-master/pages/_document.jsx
+++ url-shortening-api-master/pages/_document.jsx
@@ -12,6 +12,7 @@ class MyDocument extends Document {
                     <link rel="preconnect" href="https://fonts.googleapis.com"/>
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
                     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;700&display=swap" rel="stylesheet"/>
+                    <link rel="shortcut icon" href="images/favicon-32x32.png" type="image/x-icon" />
                 </Head>
                 <body>
                     <Main />
